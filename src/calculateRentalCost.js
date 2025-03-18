d4748a6db828b2d922925da60b50cedc97585f44
lidiia-tsymborovych <lidacimbirovic@gmail.com>
2025-03-18 23:06:56 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dayRent = 40;
+  let totalCost = dayRent * days;
+
+  if (days >= 3 && days < 7) {
+    totalCost -= 20;
+  } else if (days >= 7) {
+    totalCost -= 50;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
